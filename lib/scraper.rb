206e1009f5f833ea1f9704d5aaa24593644de38f
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-02 13:30:55 -0400

    initial scraper setup

---
+++ lib/scraper.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+def top_anime_scraper
+    
+    html = open(https://myanimelist.net)
+    doc = Nokogiri::HTML(html)
+    binding.pry
+
+end
+
+top_anime_scraper
