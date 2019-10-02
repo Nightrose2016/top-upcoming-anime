096cb8a9672c21b005bf0c30343dec3ce115ac97
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-02 17:13:13 -0400

    feature scrape all upcoming anime method defined

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,8 @@ def top_upcoming_anime_scraper
     doc = Nokogiri::HTML(html)
     doc.css(".ranking-digest")[6].css(".ranking-unit").css(".data").each { |anime| 
         title = anime.css(".title")
-        url =  anime.css(".title a"). attr("href").value
-        Movie.new(title, url)
+        url =  anime.css(".title a").attr("href").value
+        Anime.new(title, url)
     }
     #binding.pry
 
@@ -20,6 +20,10 @@ end
 def upcoming_anime_scraper
     html = open("https://myanimelist.net/anime/season")
     doc = Nokogiri::HTML(html)
+    doc.css(".js-categories-seasonal").each { |anime|
+        title = anime.css(".title-text")
+        url = anime.css(".title-text a").attr("href").value
+        Anime.new(title, url)
     binding.pry
 end
 
