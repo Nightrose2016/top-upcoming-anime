ac5353ea91f3425551cc4688e0e30626002dfda8
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-03 17:06:41 -0400

    bug fixes

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -6,13 +6,13 @@ class CLI
         puts '"Please input: most anticipated" or "all seasonal".' 
         while user_input != 'exit'
             user_input = gets.chomp
-        case user_input.downcase
+        case user_input
         when "most anticipated"
             Scraper.top_upcoming_anime_scraper
-            Anime.all.each.with_index { |anime, index| puts "#{index} #{anime.title}"
+            Anime.all.each.with_index { |anime, index| puts "#{index} #{anime.title}" }
         when "all seasonal"
-            Scraper.upcoming_anime_scraper
-            Anime.all.each.with_index { |anime, index| puts "#{index} #{anime.title}"
+                Scraper.upcoming_anime_scraper
+                Anime.all.each.with_index { |anime, index| puts "#{index} #{anime.title}" }
         end
         menu
     end
@@ -23,19 +23,18 @@ class CLI
         Anime.all[input.to_i = 1]
 
         if !anime
-         puts "Anime not found.  Please select a different movie!"
-         menu
+            puts "Anime not found.  Please select a different movie!"
+            menu
         else
             Scraper.scrape_anime_profile(anime)
             puts "Here are the details for #{anime.title}:"
-            puts "Status: #{status}"
-            puts "Aired: #{aired}"
-            puts "Broadcast #{broadcast}"
-            puts "Studio(s) #{studio}"
-            puts "Genre #{genre}"
-            puts "Duration #{duration}"
-            puts "Rating #{rating}"
+            puts "Status: #{Anime.status}"
+            puts "Aired: #{Anime.aired}"
+            puts "Broadcast #{Anime.broadcast}"
+            puts "Studio(s) #{Anime.studio}"
+            puts "Genre #{Anime.genre}"
+            puts "Duration #{Anime.duration}"
+            puts "Rating #{Anime.rating}"
         end
     end
-    
 end
