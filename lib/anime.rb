337a20f4723a7707cffbccc15021d13e824b135f
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-03 14:33:15 -0400

    added to attr_acc and removed excess white space

--- lib/anime.rb
+++ lib/anime.rb
@@ -1,14 +1,18 @@
 class Anime
-  attr_accessor :title, :url
+  attr_accessor :title, :url, :status, :aired, :broadcast, :studio, :genre, :duration, :rating
   @@all = []
 
-    def initialize(title, url)
-      @title = title
-      @url = url
-      @@all << self
-    end
+  def initialize(title, url)
+    @title = title
+    @url = url
+    @@all << self
+  end
 
-    def self.all
-        @@all
-    end
+  def self.all
+      @@all
+  end
+
+  def status(status)
+    @status = status
+  end
 end
