de047e70ccbe6324b1d9dfd3dfe07a991e74aaca
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-03 12:45:14 -0400

    added user selection of top or all seasonal anime

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,7 +2,19 @@ class CLI
     def run
 
         puts "Welcome to your seasonal anime database."
-        puts "What anime are you looking to see?"
+        puts "would you like to see this seasons most anticipated anime or all the upcoming seasonal anime?"
+        puts '"Most anticipated" or "all seasonal".' 
+        while user_input != 'exit'
+            user_input = gets.chomp
+        case user_input.downcase
+        when "most anticipated"
+            Scraper.top_upcoming_anime_scraper
+            Anime.all.each.with_index { |anime, index| puts "#{index} #{anime.title}"
+        when "all seasonal"
+            Scraper.upcoming_anime_scraper
+            Anime.all.each.with_index { |anime, index| puts "#{index} #{anime.title}"
+        end
+        
     end
     
 end
