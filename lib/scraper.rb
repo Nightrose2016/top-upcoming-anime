cdd523aee1db2fd438a6b38f8e506889a446a9de
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-03 17:20:40 -0400

    bug fixes and made scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,46 +1,45 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+class Scraper
+    def top_upcoming_anime_scraper
 
-def top_upcoming_anime_scraper
+        html = open("https://myanimelist.net/")
+        doc = Nokogiri::HTML(html)
+        doc.css(".ranking-digest")[6].css(".ranking-unit").css(".data").each { |anime| 
+            title = anime.css(".title").text.strip
+            url =  anime.css(".title").attr("href").value
+            Anime.new(title, url)}
+        #binding.pry
 
-    html = open("https://myanimelist.net/")
-    doc = Nokogiri::HTML(html)
-    doc.css(".ranking-digest")[6].css(".ranking-unit").css(".data").each { |anime| 
-        title = anime.css(".title").text.strip
-        url =  anime.css(".title").attr("href").value
-        Anime.new(title, url)
-    }
-    #binding.pry
+    end
 
-end
+    #top_upcoming_anime_scraper
 
-#top_upcoming_anime_scraper
+    def upcoming_anime_scraper
+        html = open("https://myanimelist.net/anime/season")
+        doc = Nokogiri::HTML(html)
+        doc.css(".js-categories-seasonal").each { |anime|
+            title = doc.css(".js-categories-seasonal").css(".title-text").text.strip
+            url = anime.css(".title-text a").attr("href").value 
+            Anime.new(title, url)}
+        # binding.pry
+    end
 
-def upcoming_anime_scraper
-    html = open("https://myanimelist.net/anime/season")
-    doc = Nokogiri::HTML(html)
-    doc.css(".js-categories-seasonal").each { |anime|
-        title = doc.css(".js-categories-seasonal").css(".title-text").text.strip
-        url = anime.css(".title-text" a).attr("href")
-        Anime.new(title, url)
-    }
-    # binding.pry
-end
+    upcoming_anime_scraper
 
- upcoming_anime_scraper
-
-def scrape_anime_profile
-    html = open(anime.url)
-    doc = Nokogiri::HTML(html)
-    Anime.status =  
-    Anime.aired = 
-    Anime.broadcast =
-    Anime.studio =
-    Anime.genre =
-    Anime.duration =
-    Anime.rating =
-    #binding.pry
+    def scrape_anime_profile
+        html = open(Anime.url)
+        doc = Nokogiri::HTML(html)
+        # Anime.status =  
+        # Anime.aired = 
+        # Anime.broadcast =
+        # Anime.studio =
+        # Anime.genre =
+        # Anime.duration =
+        # Anime.rating =
+        # binding.pry
+    end
 end
 
 # scrape_anime_profile
