a3fdaa21889d746e65d301739f4e4b2141235477
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-03 20:43:43 -0400

    added feature to make easier to read

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,10 +1,12 @@
 require 'pry'
+require 'colorize'
 class CLI
     def run
         user_input = nil
         puts "Welcome to your seasonal anime database."
         puts "Would you like to see this season's most anticipated anime or all the upcoming seasonal anime?"
-        puts 'Please input: "most anticipated" or "all seasonal".' 
+        puts 'Please input: "most anticipated" or "all seasonal".'.colorize(:green) 
+        puts ""
         while user_input != 'exit'
             user_input = gets.chomp
         case user_input
@@ -20,12 +22,13 @@ class CLI
     end
 
     def menu
-        puts "Please select an anime to get it's details"
+        puts ""
+        puts "Please select an anime to get it's details".colorize(:green)
         input = gets.chomp
         anime = Anime.all[input.to_i-1]
 
         if !anime
-            puts "Anime not found.  Please select a different movie!"
+            puts "Anime not found.  Please select a different movie!".colorize(:red)
             menu
         else
             Scraper.scrape_anime_profile(anime)
