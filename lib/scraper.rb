3262d0142072de4a911cde972acb6c826dfc32bc
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-10-08 12:04:28 -0400

    removed features. and redundant feature

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -46,32 +46,23 @@ class Scraper
     def self.scrape_anime_profile(anime)
         html = open(anime.url)
         doc = Nokogiri::HTML(html)
-        anime.status = doc.css(".borderClass").css("div")[11].text.gsub("\n", "").strip
         anime.aired =  doc.css(".borderClass").css(".spaceit")[7].text.split("Aired:")[1].strip
-        # anime.broadcast = doc.css(".borderClass").css(".spaceit")[8].text.split("Broadcast:")[1].strip
         anime.duration = doc.css(".borderClass").css(".spaceit")[11].text.split("Duration:")[1].strip
         if doc.css(".borderClass").css("div")[9].css("a").text == "Movie"
-            # anime.aired = doc.css(".borderClass").css(".spaceit")[7].text.split("Aired:")[1].strip
-            anime.studio = doc.css(".borderClass").css("div")[15].css("a").text
+            # anime.studio = doc.css(".borderClass").css("div")[15].css("a").text
             anime.broadcast = "this is a movie with no schedualed broadcast"
-            anime.genre =  doc.css(".borderClass").css("div")[17].text.split("\n")[2]
-            anime.rating = doc.css(".borderClass").css("div")[19].text.gsub("\n", "").strip
-            # anime.duration = doc.css(".borderClass").css("div")[18].text.gsub("\n", "").strip
-            # anime.status = doc.css(".borderClass").css("div")[11].text.gsub("\n", "").strip
+            # anime.genre =  doc.css(".borderClass").css("div")[17].text.split("\n")[2]
+            # anime.rating = doc.css(".borderClass").css("div")[19].text.gsub("\n", "").strip
             binding.pry
         # elsif doc.css(".borderClass").css("div")[9].text.split("Type:")[1].strip == "TV"
-        #     anime.status = doc.css(".borderClass").css("div")[11].text.gsub("\n", "").strip
         #     anime.aired =  doc.css(".borderClass").css(".spaceit")[7].text.split("Aired:")[1].strip
         #     anime.broadcast = doc.css(".borderClass").css(".spaceit")[8].text.split("Broadcast:")[1].strip
         #     anime.duration = doc.css(".borderClass").css(".spaceit")[11].text.split("Duration:")[1].strip
 
         else    
-            # anime.status = doc.css(".borderClass").css("div")[12].text.gsub("\n", "").strip
-            # anime.aired = doc.css(".borderClass").css(".spaceit")[7].text.split("Aired:")[1].strip
-            anime.broadcast = doc.css(".borderClass").css("div")[15].text.gsub("\n", "").strip
-            anime.studio = doc.css(".borderClass").css("div")[18].text.gsub("\n", "").strip
+            anime.broadcast = doc.css(".borderClass").css(".spaceit")[8].text.split("Broadcast:")[1].strip
+            # anime.studio = doc.css(".borderClass").css("div")[18].text.gsub("\n", "").strip
             anime.genre = doc.css(".borderClass").css("div")[20].text.gsub("\n", "").strip
-            anime.duration = doc.css(".borderClass").css("div")[21].text.gsub("\n", "").strip
             anime.rating = doc.css(".borderClass").css("div")[22].text.gsub("\n", "").strip
             binding.pry
         end
